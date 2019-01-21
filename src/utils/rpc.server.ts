daac6b796f7c11fb749a2a5a8ff4f96fc8ffd4b4
Ron Borysovski <roni537@gmail.com>
2019-01-21 17:32:27 +0200

    refactor: Return channels as map of <id, IChannel>

--- src/utils/rpc.server.ts
+++ src/utils/rpc.server.ts
@@ -1,9 +1,18 @@
 import { ChannelManager } from '../channel/channel.manager';
+import { IChannel } from '../channel/channel.interface';
 
 const jayson = require('jayson/promise');
 
 export const RPCServer = new jayson.Server({
-    getChannelsByIds(ids: string[]) {
-        return ChannelManager.getByIds(ids);
+    async getChannelsByIds(ids: string[]) {
+        const channels = await ChannelManager.getByIds(ids);
+
+        const channelsMap: { [id: string]: IChannel } = {};
+
+        channels.forEach((channel: IChannel) => {
+            channelsMap[channel.id!] = channel;
+        });
+
+        return channelsMap;
     },
 });
