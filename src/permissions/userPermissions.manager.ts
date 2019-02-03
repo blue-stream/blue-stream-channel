f8a49845047dbbf098ed4713cb38bcc49924737e
Almog Vagman Ciprut <almogvc@gmail.com>
2019-02-03 17:04:51 +0200

    refactor: Add userPermissions manager

---
+++ src/permissions/userPermissions.manager.ts
@@ -0,0 +1,86 @@
+import { IUserPermissions, PermissionTypes } from './userPermissions.interface';
+
+import { UserPermissionsRepository } from './userPermissions.repository';
+import { UnauthorizedUserError, ChannelNotFoundError } from '../utils/errors/userErrors';
+import { IChannel } from '../channel/channel.interface';
+import { ChannelManager } from '../channel/channel.manager';
+
+export class UserPermissionsManager {
+
+    static async create(userPermissions: IUserPermissions, requestingUser: string) {
+        const returnedResults = await Promise.all([
+            UserPermissionsManager.isUserAdmin(requestingUser, userPermissions.channel),
+            ChannelManager.getById(userPermissions.channel),
+        ]);
+
+        const [isRequestingUserAdmin, channel] = returnedResults;
+
+        if (channel) {
+            if (isRequestingUserAdmin) {
+                return UserPermissionsRepository.create(userPermissions);
+            }
+
+            throw new UnauthorizedUserError();
+        }
+
+        throw new ChannelNotFoundError();
+    }
+
+    static async updateOne(requestingUser: string, user: string, channel: string, permissions: PermissionTypes[]) {
+        const isRequestingUserAdmin: boolean = await UserPermissionsManager.isUserAdmin(requestingUser, channel);
+
+        if (isRequestingUserAdmin) {
+            return UserPermissionsRepository.updateOne(user, channel, permissions);
+        }
+
+        throw new UnauthorizedUserError();
+    }
+
+    static async deleteOne(requestingUser: string, user: string, channel: string) {
+        const isRequestingUserAdmin: boolean = await UserPermissionsManager.isUserAdmin(requestingUser, channel);
+
+        if (isRequestingUserAdmin) {
+            return UserPermissionsRepository.deleteOne(user, channel);
+        }
+
+        throw new UnauthorizedUserError();
+    }
+
+    static async getOne(requestingUser: string, user: string, channel: string) {
+        const returnedResults = await Promise.all([
+            UserPermissionsManager.isUserAdmin(requestingUser, channel),
+            UserPermissionsRepository.getOne(user, channel),
+        ]);
+
+        const [isRequestingUserAdmin, userPermissions] = returnedResults;
+
+        if (isRequestingUserAdmin) {
+            return userPermissions;
+        }
+
+        throw new UnauthorizedUserError();
+    }
+
+    static async getMany(requestingUser: string, userPermissions: Partial<IUserPermissions>, startIndex?: number, endIndex?: number, sortOrder?: '-' | '', sortBy?: string) {
+        const returnedResults = await Promise.all([
+            // UserPermissionsManager.isUserAdmin(requestingUser, userPermissions.channel),
+            UserPermissionsRepository.getMany(userPermissions, startIndex, endIndex, sortOrder, sortBy),
+        ]);
+
+        return UserPermissionsRepository.getMany(userPermissions, startIndex, endIndex, sortOrder, sortBy);
+    }
+
+    static getAmount(channelFilter: Partial<IUserPermissions>) {
+        return UserPermissionsRepository.getAmount(channelFilter);
+    }
+
+    static async isUserAdmin(user: string, channel: string): Promise<boolean> {
+        const requestingUserPremissions: IUserPermissions | null = await UserPermissionsRepository.getOne(user, channel);
+
+        if (requestingUserPremissions && requestingUserPremissions.permissions.indexOf(PermissionTypes.Admin) === -1) {
+            return true;
+        }
+
+        return false;
+    }
+}
