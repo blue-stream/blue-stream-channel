9bfec51b4e107c73e6725ab86e36a31582cd41db
Almog Vagman Ciprut <almogvc@gmail.com>
2019-02-09 21:24:08 +0200

    refactor: Change route for userPermissions

--- src/permissions/userPermissions.router.ts
+++ src/permissions/userPermissions.router.ts
@@ -3,15 +3,15 @@ import { UserPermissionsValidator } from './validator/userPermissions.validator'
 import { UserPermissionsController } from './userPermissions.contoller';
 import { Wrapper } from '../utils/wrapper';
 
-const ChannelRouter: Router = Router();
+const UserPermissionsRouter: Router = Router();
 
-ChannelRouter.post('/', UserPermissionsValidator.canCreate, Wrapper.wrapAsync(UserPermissionsController.create));
-ChannelRouter.put('/', UserPermissionsValidator.canUpdateOne, Wrapper.wrapAsync(UserPermissionsController.updateOne));
-ChannelRouter.delete('/', UserPermissionsValidator.canDeleteOne, Wrapper.wrapAsync(UserPermissionsController.deleteOne));
-ChannelRouter.get('/one', UserPermissionsValidator.canGetOne, Wrapper.wrapAsync(UserPermissionsController.getOne));
-ChannelRouter.get('/channels', UserPermissionsValidator.canGetUserPermittedChannels, Wrapper.wrapAsync(UserPermissionsController.getUserPermittedChannels));
-ChannelRouter.get('/:channelId/users', UserPermissionsValidator.canGetChannelPermittedUsers, Wrapper.wrapAsync(UserPermissionsController.getChannelPermittedUsers));
-ChannelRouter.get('/channels/amount', UserPermissionsValidator.canGetUserPermittedChannelsAmount, Wrapper.wrapAsync(UserPermissionsController.getUserPermittedChannelsAmount));
-ChannelRouter.get('/:channelId/users/amount', UserPermissionsValidator.canGetChannelPermittedUsersAmount, Wrapper.wrapAsync(UserPermissionsController.getChannelPermittedUsersAmount));
+UserPermissionsRouter.post('/', UserPermissionsValidator.canCreate, Wrapper.wrapAsync(UserPermissionsController.create));
+UserPermissionsRouter.put('/', UserPermissionsValidator.canUpdateOne, Wrapper.wrapAsync(UserPermissionsController.updateOne));
+UserPermissionsRouter.delete('/', UserPermissionsValidator.canDeleteOne, Wrapper.wrapAsync(UserPermissionsController.deleteOne));
+UserPermissionsRouter.get('/one', UserPermissionsValidator.canGetOne, Wrapper.wrapAsync(UserPermissionsController.getOne));
+UserPermissionsRouter.get('/channels', UserPermissionsValidator.canGetUserPermittedChannels, Wrapper.wrapAsync(UserPermissionsController.getUserPermittedChannels));
+UserPermissionsRouter.get('/:channelId/users', UserPermissionsValidator.canGetChannelPermittedUsers, Wrapper.wrapAsync(UserPermissionsController.getChannelPermittedUsers));
+UserPermissionsRouter.get('/channels/amount', UserPermissionsValidator.canGetUserPermittedChannelsAmount, Wrapper.wrapAsync(UserPermissionsController.getUserPermittedChannelsAmount));
+UserPermissionsRouter.get('/:channelId/users/amount', UserPermissionsValidator.canGetChannelPermittedUsersAmount, Wrapper.wrapAsync(UserPermissionsController.getChannelPermittedUsersAmount));
 
-export { ChannelRouter };
+export { UserPermissionsRouter };
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { ChannelRouter } from './channel/channel.router';
+import { UserPermissionsRouter } from './permissions/userPermissions.router';
 
 const AppRouter: Router = Router();
 
 AppRouter.use('/api/channel', ChannelRouter);
+AppRouter.use('/api/userPermissions', UserPermissionsRouter);
 
 export { AppRouter };
