d0891ddebc6e1855ab358d0b99ca8607f356faad
Almog Vagman Ciprut <almogvc@gmail.com>
2019-02-14 15:12:47 +0200

    refactor: Add uniqe option to userPermissions schema's indexes

--- src/permissions/userPermissions.model.ts
+++ src/permissions/userPermissions.model.ts
@@ -36,6 +36,6 @@ const userPermissionsSchema: mongoose.Schema = new mongoose.Schema(
         },
     });
 
-userPermissionsSchema.index({ channel: 1, user: -1 });
+userPermissionsSchema.index({ channel: 1, user: -1 }, { unique: true });
 
 export const userPermissionsModel = mongoose.model<IUserPermissions & mongoose.Document>('UserPermissions', userPermissionsSchema);
