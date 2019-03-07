6c5a618c1001e48f9bf68581cf3f8e0d853fe88e
Yuval Assa <assa.yuval107@gmail.com>
2019-03-07 11:26:58 +0200

    feat: Add health check

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { ChannelRouter } from './channel/channel.router';
 import { UserPermissionsRouter } from './permissions/userPermissions.router';
+import { HealthRouter } from './utils/health/health.router';
 
 const AppRouter: Router = Router();
 
 AppRouter.use('/api/channel', ChannelRouter);
 AppRouter.use('/api/userPermissions', UserPermissionsRouter);
+AppRouter.use('/health', HealthRouter);
 
 export { AppRouter };
